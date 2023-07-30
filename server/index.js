5256584495f021f9d2e3e054c07f3d9d9d3e7834
rancelazavedo <azavedorancel@gmail.com>
2023-07-30 16:38:26 +0530

    reverting back to previous version

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-  "mongodb+srv://user123:<password>@cluster0.evo7a7c.mongodb.net/merchappdb?retryWrites=true&w=majority"
+  "mongodb+srv://user123:hitohitonomi@cluster0.evo7a7c.mongodb.net/merchappdb?retryWrites=true&w=majority"
 );
 
 app.get("/getUsers", async (req, res) => {
